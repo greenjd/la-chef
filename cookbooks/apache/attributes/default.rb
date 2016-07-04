722b7c9ccb88f3894c7a0f15c7a1c6512fad8611
root <root@john-thegreens-co-uk1.mylabserver.com>
2016-07-04 13:32:30 +0000

    updating for lessons

--- cookbooks/apache/attributes/default.rb
+++ cookbooks/apache/attributes/default.rb
@@ -1,6 +1,7 @@
 default["apache"]["sites"]["john2"] = { "site_title" => "John's website soon arriving", "port" => 80, "domain" => "john-thegreens-co-uk2.mylabserver.com" }
 default["apache"]["sites"]["john2b"] = { "site_title" => "John's second website soon arriving", "port" => 80, "domain" => "john-thegreens-co-uk2b.mylabserver.com" }
 default["apache"]["sites"]["john3"] = { "site_title" => "John's third website soon arriving", "port" => 80, "domain" => "john-thegreens-co-uk3.mylabserver.com" }
+default["author"]["name"] = "John Green"
 case node["platform"] 
 	when "centos"
 		default["apache"]["package"] = "httpd"
